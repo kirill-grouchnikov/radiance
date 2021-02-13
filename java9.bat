a419403aae699ae2e0e8555cae2faba196734362
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2021-02-12 21:01:03 -0500

    Add a script to set Java 9 on Windows

---
+++ java9.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\Program Files\Java\jdk-9.0.4
+echo setting PATH
+set PATH=C:\Program Files\Java\jdk-9.0.4\bin;%PATH%
+echo Display java version
+java -version
