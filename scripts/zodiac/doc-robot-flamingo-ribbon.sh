05a3ee4e6a4242c0291b52d168f003d0ae201155
Kirill Grouchnikov <kirillg@google.com>
2021-06-17 09:29:57 -0400

    Update Zodiac scripts

--- scripts/zodiac/doc-robot-flamingo-ribbon.sh
+++ scripts/zodiac/doc-robot-flamingo-ribbon.sh
@@ -31,8 +31,8 @@ export JAVA_HOME=`/usr/libexec/java_home -v 9`
 alias JAVA="java"
 
 RADIANCE_VERSION=4.0.0
-KOTLIN_VERSION=1.4.30
-KOTLIN_COROUTINES_VERSION=1.4.2
+KOTLIN_VERSION=1.5.10
+KOTLIN_COROUTINES_VERSION=1.5.0
 CLASSPATH=../../drop/$RADIANCE_VERSION/tools/radiance-zodiac-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/tools/radiance-tools-common-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/core/radiance-substance-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/core/radiance-flamingo-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/core/radiance-substance-extras-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/core/radiance-neon-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/core/radiance-trident-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/demo/radiance-flamingo-demo-$RADIANCE_VERSION.jar:../../drop/$RADIANCE_VERSION/core/radiance-substance-extras-$RADIANCE_VERSION.jar:../../build/libs-tools/jgoodies-forms-1.9.0.jar:../../build/libs-tools/jgoodies-common-1.8.1.jar:../../build/libs-tools/kotlin-stdlib-$KOTLIN_VERSION.jar:../../build/libs-tools/kotlin-stdlib-common-$KOTLIN_VERSION.jar:../../build/libs-tools/kotlinx-coroutines-core-jvm-$KOTLIN_COROUTINES_VERSION.jar:../../build/libs-tools/kotlinx-coroutines-swing-$KOTLIN_COROUTINES_VERSION.jar
 
 JAVA -cp $CLASSPATH org.pushingpixels.tools.zodiac.RobotMain org.pushingpixels.tools.zodiac.flamingo.skins.Autumn ../../docs/images/
