212bc6d4f48501e43fa0ce6967625532e30376bb
Kirill Grouchnikov <kirillg@google.com>
2021-12-15 17:04:37 -0500

    Cleanups
    * Remove unused methods and constants in font policy class from JGoodies
    * Darcula has been inactive in the last three years - remove from benchmark
    * Update the benchmark script to use the latest FlatLaf version

--- scripts/laf-benchmark/auto-dynamic-flatlaf.sh
+++ scripts/laf-benchmark/auto-dynamic-flatlaf.sh
@@ -32,6 +32,6 @@ alias JAVA="java"
 
 RADIANCE_VERSION=6.0-SNAPSHOT
 CLASSPATH=../../drop/$RADIANCE_VERSION/tools/radiance-laf-benchmark-$RADIANCE_VERSION.jar:../../build/libs-tools/jgoodies-forms-1.9.0.jar:../../build/libs-tools/jgoodies-common-1.8.1.jar
-FLATLAF_CLASSPATH=../../../thirdparty/flatlaf-1.6.jar
+FLATLAF_CLASSPATH=../../../thirdparty/flatlaf-1.6.5.jar
 
 java -Dswing.defaultlaf=com.formdev.flatlaf.FlatLightLaf -cp $CLASSPATH:$FLATLAF_CLASSPATH org.pushingpixels.radiance.tools.lafbenchmark.DynamicPerformanceSuite 10
