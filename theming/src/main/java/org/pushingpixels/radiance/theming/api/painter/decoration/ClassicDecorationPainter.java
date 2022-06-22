e001cc15724c934bb39cfee5f43cd2d2dedef6e2
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2022-06-22 16:47:38 -0400

    Remove image caching in classic decoration painter

--- theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/decoration/ClassicDecorationPainter.java
+++ theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/decoration/ClassicDecorationPainter.java
@@ -1,63 +1,52 @@
 /*
  * Copyright (c) 2005-2022 Radiance Kirill Grouchnikov. All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- *  o Redistributions of source code must retain the above copyright notice, 
- *    this list of conditions and the following disclaimer. 
- *     
- *  o Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
- *    and/or other materials provided with the distribution. 
- *     
+ *
+ *  o Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
  *  o Neither the name of the copyright holder nor the names of
- *    its contributors may be used to endorse or promote products derived 
- *    from this software without specific prior written permission. 
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.pushingpixels.radiance.theming.api.painter.decoration;
 
-import org.pushingpixels.radiance.common.api.RadianceCommonCortex;
-import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
 import org.pushingpixels.radiance.theming.api.RadianceSkin;
+import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
 import org.pushingpixels.radiance.theming.api.colorscheme.RadianceColorScheme;
 import org.pushingpixels.radiance.theming.api.painter.fill.ClassicFillPainter;
-import org.pushingpixels.radiance.theming.internal.utils.ImageHashMapKey;
-import org.pushingpixels.radiance.theming.internal.utils.LazyResettableHashMap;
-import org.pushingpixels.radiance.theming.internal.utils.RadianceCoreUtilities;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 /**
  * Decoration painter that paints a classic gradient. This class is part of
  * officially supported API.
- * 
+ *
  * @author Kirill Grouchnikov
  */
 public class ClassicDecorationPainter implements RadianceDecorationPainter {
 	/**
-	 * The display name for the decoraion painters of this class.
+	 * The display name for the decoration painters of this class.
 	 */
 	public static final String DISPLAY_NAME = "Classic";
-
-	/**
-	 * Cache for small objects.
-	 */
-	protected final static LazyResettableHashMap<BufferedImage> smallImageCache =
-			new LazyResettableHashMap<>("ClassicDecorationPainter");
 
 	/**
 	 * Single gradient painter instance.
@@ -81,20 +70,6 @@ public class ClassicDecorationPainter implements RadianceDecorationPainter {
                                     RadianceThemingSlices.DecorationAreaType decorationAreaType, int width, int height,
                                     RadianceSkin skin) {
 		RadianceColorScheme scheme = skin.getBackgroundColorScheme(decorationAreaType);
-		if (width * height < 100000) {
-			double scale = RadianceCommonCortex.getScaleFactor(comp);
-			ImageHashMapKey key = RadianceCoreUtilities.getScaleAwareHashKey(
-					scale, width, height, scheme.getDisplayName());
-			BufferedImage result = smallImageCache.get(key);
-			if (result == null) {
-				result = RadianceCoreUtilities.getBlankImage(scale, width, height);
-				this.internalPaint((Graphics2D) result.getGraphics(), comp, width, height, scheme);
-				smallImageCache.put(key, result);
-			}
-			RadianceCommonCortex.drawImageWithScale(graphics, scale, result, 0, 0);
-			return;
-		}
-
 		this.internalPaint(graphics, comp, width, height, scheme);
 	}
 
@@ -110,7 +85,7 @@ public class ClassicDecorationPainter implements RadianceDecorationPainter {
 
     /**
 	 * Paints the specified area.
-	 * 
+	 *
 	 * @param graphics
 	 *            Graphics context.
 	 * @param comp
