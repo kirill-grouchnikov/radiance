0acb67f876521dbbc908aeefb4f96ff385e73bf9
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2022-07-13 11:16:05 -0400

    Convert some overlay painters to 1x rendering

--- theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/overlay/TopBezelOverlayPainter.java
+++ theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/overlay/TopBezelOverlayPainter.java
@@ -1,112 +1,117 @@
 /*
  * Copyright (c) 2005-2022 Radiance Kirill Grouchnikov. All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- *  o Redistributions of source code must retain the above copyright notice, 
- *    this list of conditions and the following disclaimer. 
- *     
- *  o Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
- *    and/or other materials provided with the distribution. 
- *     
+ *
+ *  o Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
  *  o Neither the name of the copyright holder nor the names of
- *    its contributors may be used to endorse or promote products derived 
- *    from this software without specific prior written permission. 
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.pushingpixels.radiance.theming.api.painter.overlay;
 
-import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
+import org.pushingpixels.radiance.common.api.RadianceCommonCortex;
 import org.pushingpixels.radiance.theming.api.RadianceSkin;
+import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
 import org.pushingpixels.radiance.theming.api.colorscheme.ColorSchemeSingleColorQuery;
 import org.pushingpixels.radiance.theming.api.colorscheme.RadianceColorScheme;
 import org.pushingpixels.radiance.theming.internal.utils.RadianceCoreUtilities;
-import org.pushingpixels.radiance.theming.internal.utils.RadianceSizeUtils;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Line2D;
 
 /**
  * Overlay painter that paints a bezel line at the top edge of the relevant
  * decoration area. This class is part of officially supported API.
- * 
+ *
  * @author Kirill Grouchnikov
  */
 public final class TopBezelOverlayPainter implements RadianceOverlayPainter {
-	/**
-	 * Used to compute the color of the top line painted by this overlay
-	 * painter.
-	 */
-	ColorSchemeSingleColorQuery colorSchemeQueryTop;
+    /**
+     * Used to compute the color of the top line painted by this overlay
+     * painter.
+     */
+    ColorSchemeSingleColorQuery colorSchemeQueryTop;
 
-	/**
-	 * Used to compute the color of the bottom line painted by this overlay
-	 * painter.
-	 */
-	ColorSchemeSingleColorQuery colorSchemeQueryBottom;
+    /**
+     * Used to compute the color of the bottom line painted by this overlay
+     * painter.
+     */
+    ColorSchemeSingleColorQuery colorSchemeQueryBottom;
 
-	/**
-	 * Creates a new overlay painter that paints a bezel line at the top edge of
-	 * the relevant decoration area
-	 * 
-	 * @param colorSchemeQueryTop
-	 *            Used to compute the color of the top line painted by this
-	 *            overlay painter.
-	 * @param colorSchemeQueryBottom
-	 *            Used to compute the color of the top line painted by this
-	 *            overlay painter.
-	 */
-	public TopBezelOverlayPainter(
-			ColorSchemeSingleColorQuery colorSchemeQueryTop,
-			ColorSchemeSingleColorQuery colorSchemeQueryBottom) {
-		this.colorSchemeQueryTop = colorSchemeQueryTop;
-		this.colorSchemeQueryBottom = colorSchemeQueryBottom;
-	}
+    /**
+     * Creates a new overlay painter that paints a bezel line at the top edge of
+     * the relevant decoration area
+     *
+     * @param colorSchemeQueryTop    Used to compute the color of the top line painted by this
+     *                               overlay painter.
+     * @param colorSchemeQueryBottom Used to compute the color of the top line painted by this
+     *                               overlay painter.
+     */
+    public TopBezelOverlayPainter(
+            ColorSchemeSingleColorQuery colorSchemeQueryTop,
+            ColorSchemeSingleColorQuery colorSchemeQueryBottom) {
+        this.colorSchemeQueryTop = colorSchemeQueryTop;
+        this.colorSchemeQueryBottom = colorSchemeQueryBottom;
+    }
 
-	@Override
-	public void paintOverlay(Graphics2D graphics, Component comp,
-                             RadianceThemingSlices.DecorationAreaType decorationAreaType, int width, int height,
-                             RadianceSkin skin) {
-		Component topMostWithSameDecorationAreaType = RadianceCoreUtilities
-				.getTopMostParentWithDecorationAreaType(comp,
-						decorationAreaType);
+    @Override
+    public void paintOverlay(Graphics2D g, Component comp,
+            RadianceThemingSlices.DecorationAreaType decorationAreaType, int width, int height,
+            RadianceSkin skin) {
+        Component topMostWithSameDecorationAreaType = RadianceCoreUtilities
+                .getTopMostParentWithDecorationAreaType(comp,
+                        decorationAreaType);
 
-		Point inTopMost = SwingUtilities.convertPoint(comp, new Point(0, 0),
-				topMostWithSameDecorationAreaType);
-		int dy = inTopMost.y;
+        Point inTopMost = SwingUtilities.convertPoint(comp, new Point(0, 0),
+                topMostWithSameDecorationAreaType);
+        int dy = inTopMost.y;
 
-		float borderStrokeWidth = RadianceSizeUtils.getBorderStrokeWidth(comp);
-		graphics.setStroke(new BasicStroke(borderStrokeWidth));
+        Graphics2D graphics = (Graphics2D) g.create();
+        // Important - do not set KEY_STROKE_CONTROL to VALUE_STROKE_PURE, as that instructs AWT
+        // to not normalize coordinates to paint at full pixels, and will result in blurry
+        // outlines.
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
 
-		RadianceColorScheme colorScheme = skin
-				.getBackgroundColorScheme(decorationAreaType);
-		graphics.setColor(this.colorSchemeQueryTop.query(colorScheme));
-		float topY = - dy;
-		Line2D.Float topLine = new Line2D.Float(0, topY, width, topY);
-		graphics.draw(topLine);
+        RadianceCommonCortex.paintAtScale1x(graphics, 0, 0, width, height,
+                (graphics1X, x, y, scaledWidth, scaledHeight, scaleFactor) -> {
+                    RadianceColorScheme colorScheme = skin.getBackgroundColorScheme(decorationAreaType);
+                    graphics1X.setColor(this.colorSchemeQueryTop.query(colorScheme));
 
-		graphics.setColor(this.colorSchemeQueryBottom.query(colorScheme));
-		float bezelY = borderStrokeWidth - dy;
-		Line2D.Float bezelLine = new Line2D.Float(0, bezelY, width, bezelY);
-		graphics.draw(bezelLine);
-	}
+                    int topY = -(int) (scaleFactor * dy);
+                    graphics1X.drawLine(0, topY, scaledWidth, topY);
 
-	@Override
-	public String getDisplayName() {
-		return "Top Bezel";
-	}
+                    graphics1X.setColor(this.colorSchemeQueryBottom.query(colorScheme));
+
+                    int bezelY = 1 - (int) (scaleFactor * dy);
+                    graphics1X.drawLine(0, bezelY, scaledWidth, bezelY);
+                });
+
+        graphics.dispose();
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Top Bezel";
+    }
 }
--- theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/overlay/TopLineOverlayPainter.java
+++ theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/overlay/TopLineOverlayPainter.java
@@ -1,94 +1,99 @@
 /*
  * Copyright (c) 2005-2022 Radiance Kirill Grouchnikov. All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- *  o Redistributions of source code must retain the above copyright notice, 
- *    this list of conditions and the following disclaimer. 
- *     
- *  o Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
- *    and/or other materials provided with the distribution. 
- *     
+ *
+ *  o Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
  *  o Neither the name of the copyright holder nor the names of
- *    its contributors may be used to endorse or promote products derived 
- *    from this software without specific prior written permission. 
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.pushingpixels.radiance.theming.api.painter.overlay;
 
-import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
+import org.pushingpixels.radiance.common.api.RadianceCommonCortex;
 import org.pushingpixels.radiance.theming.api.RadianceSkin;
+import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
 import org.pushingpixels.radiance.theming.api.colorscheme.ColorSchemeSingleColorQuery;
 import org.pushingpixels.radiance.theming.api.colorscheme.RadianceColorScheme;
 import org.pushingpixels.radiance.theming.internal.utils.RadianceCoreUtilities;
-import org.pushingpixels.radiance.theming.internal.utils.RadianceSizeUtils;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Line2D;
 
 /**
  * Overlay painter that paints a single line at the top edge of the relevant
  * decoration area. This class is part of officially supported API.
- * 
+ *
  * @author Kirill Grouchnikov
  */
 public final class TopLineOverlayPainter implements RadianceOverlayPainter {
-	/**
-	 * Used to compute the color of the line painted by this overlay painter.
-	 */
-	ColorSchemeSingleColorQuery colorSchemeQuery;
+    /**
+     * Used to compute the color of the line painted by this overlay painter.
+     */
+    ColorSchemeSingleColorQuery colorSchemeQuery;
 
-	/**
-	 * Creates a new overlay painter that paints a single line at the top edge
-	 * of the relevant decoration area
-	 * 
-	 * @param colorSchemeQuery
-	 *            Used to compute the color of the line painted by this overlay
-	 *            painter.
-	 */
-	public TopLineOverlayPainter(ColorSchemeSingleColorQuery colorSchemeQuery) {
-		this.colorSchemeQuery = colorSchemeQuery;
-	}
+    /**
+     * Creates a new overlay painter that paints a single line at the top edge
+     * of the relevant decoration area
+     *
+     * @param colorSchemeQuery Used to compute the color of the line painted by this overlay
+     *                         painter.
+     */
+    public TopLineOverlayPainter(ColorSchemeSingleColorQuery colorSchemeQuery) {
+        this.colorSchemeQuery = colorSchemeQuery;
+    }
 
-	@Override
-	public void paintOverlay(Graphics2D graphics, Component comp,
-                             RadianceThemingSlices.DecorationAreaType decorationAreaType, int width, int height,
-                             RadianceSkin skin) {
-		Component topMostWithSameDecorationAreaType = RadianceCoreUtilities
-				.getTopMostParentWithDecorationAreaType(comp, decorationAreaType);
+    @Override
+    public void paintOverlay(Graphics2D g, Component comp,
+            RadianceThemingSlices.DecorationAreaType decorationAreaType, int width, int height,
+            RadianceSkin skin) {
+        Component topMostWithSameDecorationAreaType = RadianceCoreUtilities
+                .getTopMostParentWithDecorationAreaType(comp, decorationAreaType);
+        Point inTopMost = SwingUtilities.convertPoint(comp, new Point(0, 0),
+                topMostWithSameDecorationAreaType);
+        int dy = inTopMost.y;
 
-		Point inTopMost = SwingUtilities.convertPoint(comp, new Point(0, 0),
-				topMostWithSameDecorationAreaType);
-		int dy = inTopMost.y;
+        Graphics2D graphics = (Graphics2D) g.create();
+        // Important - do not set KEY_STROKE_CONTROL to VALUE_STROKE_PURE, as that instructs AWT
+        // to not normalize coordinates to paint at full pixels, and will result in blurry
+        // outlines.
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
 
-		float borderStrokeWidth = RadianceSizeUtils.getBorderStrokeWidth(comp);
-		graphics.setStroke(new BasicStroke(borderStrokeWidth));
+        RadianceCommonCortex.paintAtScale1x(graphics, 0, 0, width, height,
+                (graphics1X, x, y, scaledWidth, scaledHeight, scaleFactor) -> {
+                    RadianceColorScheme colorScheme = skin.getBackgroundColorScheme(decorationAreaType);
+                    graphics1X.setColor(this.colorSchemeQuery.query(colorScheme));
 
-		RadianceColorScheme colorScheme = skin
-				.getBackgroundColorScheme(decorationAreaType);
-		graphics.setColor(this.colorSchemeQuery.query(colorScheme));
-		float topY = borderStrokeWidth - dy;
-		Line2D.Float line = new Line2D.Float(0, topY, width, topY);
-		graphics.draw(line);
-	}
+                    int topY = 1 - (int) (scaleFactor * dy);
+                    graphics1X.drawLine(0, topY, scaledWidth, topY);
+                });
 
-	@Override
-	public String getDisplayName() {
-		return "Top Line";
-	}
+        graphics.dispose();
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Top Line";
+    }
 
 }
