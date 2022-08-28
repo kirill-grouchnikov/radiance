1605a93c33e85e9c71707c2ae0f3ae1d04b72f71
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2022-08-28 16:51:16 -0400

    Extract emulated specular highlight into its own method

--- theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/fill/StandardFillPainter.java
+++ theming/src/main/java/org/pushingpixels/radiance/theming/api/painter/fill/StandardFillPainter.java
@@ -31,16 +31,11 @@ package org.pushingpixels.radiance.theming.api.painter.fill;
 
 import org.pushingpixels.radiance.common.api.RadianceCommonCortex;
 import org.pushingpixels.radiance.theming.api.colorscheme.RadianceColorScheme;
-import org.pushingpixels.radiance.theming.internal.utils.HashMapKey;
-import org.pushingpixels.radiance.theming.internal.utils.LazyResettableHashMap;
 import org.pushingpixels.radiance.theming.internal.utils.RadianceColorUtilities;
 import org.pushingpixels.radiance.theming.internal.utils.RadianceCoreUtilities;
 
 import java.awt.*;
 import java.awt.MultipleGradientPaint.CycleMethod;
-import java.awt.color.ColorSpace;
-import java.awt.color.ICC_ColorSpace;
-import java.awt.color.ICC_Profile;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.image.*;
@@ -96,113 +91,13 @@ public class StandardFillPainter implements RadianceFillPainter {
 
         // long millis003 = 0, millis004 = 0, millis005 = 0;
         if ((topShineColor != null) && (bottomShineColor != null)) {
-            graphics.clip(contour);
-
             int iw = (int) width;
             int ih = (int) height;
 
             int shineWidth = iw / SCALE;
             int shineHeight = ih / (2 * SCALE);
-            BufferedImage shineImage = RadianceCoreUtilities.getBlankUnscaledImage(shineWidth, shineHeight);
-            double gap = 0.5 * RadianceCommonCortex.getScaleFactor(comp) / SCALE;
-            double ramp = 2 * RadianceCommonCortex.getScaleFactor(comp) / SCALE;
-
-            double topLeftCornerRadius = 0;
-            double topRightCornerRadius = 0;
-            if (contour instanceof RoundRectangle2D) {
-                // This matches the logic in RadianceOutlineUtilities.getBaseOutline
-                RoundRectangle2D rrContour = (RoundRectangle2D) contour;
-                topLeftCornerRadius = rrContour.getArcWidth() / (2.0 * SCALE);
-                topRightCornerRadius = rrContour.getArcWidth() / (2.0 * SCALE);
-            } else if (contour instanceof Ellipse2D) {
-                // This matches the logic in BladeIconUtils.drawRadioButton
-                Ellipse2D ellContour = (Ellipse2D) contour;
-                topLeftCornerRadius = ellContour.getWidth() / (2.0 * SCALE);
-                topRightCornerRadius = ellContour.getWidth() / (2.0 * SCALE);
-            }
-
-            WritableRaster shineRaster = shineImage.getRaster();
-
-            int[] inPixels = new int[shineWidth];
-            for (int row = 0; row < shineHeight; row++) {
-                if (row <= gap) {
-                    // Leading vertical gap
-                    for (int col = 0; col < shineWidth; col++) {
-                        inPixels[col] = 0x00000000;
-                    }
-                } else {
-                    // Get the interpolated shine color for this row
-                    int rowColor = RadianceColorUtilities.getInterpolatedRGB(
-                            topShineColor, bottomShineColor, 1.0 - (double) row / (double) shineHeight);
-                    int rowAlpha = (rowColor >>> 24) & 0xFF;
-                    int rowRed = (rowColor >>> 16) & 0xFF;
-                    int rowGreen = (rowColor >>> 8) & 0xFF;
-                    int rowBlue = (rowColor >>> 0) & 0xFF;
-
-                    // Compute the y-based alpha for all the pixels in this row
-                    double yalpha;
-                    if (row <= (gap + ramp)) {
-                        // Quick ramp-up
-                        double cfraction = (row - gap) / ramp;
-                        yalpha = spline(0.0, 0.1, 0.9, 1.0, cfraction);
-                    } else {
-                        // slower ramp-down
-                        double cfraction = (row - gap - ramp) / (shineHeight - gap - ramp);
-                        yalpha = spline(0.0, 0.1, 0.9, 1.0, 1.0 - cfraction);
-                    }
-
-                    // For each column in this row, compute its x-based alpha
-                    for (int col = 0; col < shineWidth; col++) {
-                        // x-alpha is based on the distance from left / right edges
-                        double xalpha = 1.0;
-                        if (col <= shineWidth / 2) {
-                            // closer to the left edge
-                            double overlayXStart = gap;
-                            if ((topLeftCornerRadius > 0.0) && (row <= (gap + topLeftCornerRadius))) {
-                                // We are within the vertical span of the top-left corner
-                                double dy = gap + topLeftCornerRadius - row;
-                                double dx = Math.sqrt(topLeftCornerRadius * topLeftCornerRadius - dy * dy);
-                                overlayXStart = gap + topLeftCornerRadius - dx;
-                            }
-                            if (col <= overlayXStart) {
-                                // leading horizontal gap
-                                xalpha = 0.0;
-                            } else if (col <= (overlayXStart + ramp)) {
-                                // ramp-up to full alpha horizontally
-                                double cfraction = (overlayXStart + ramp - col) / ramp;
-                                xalpha = spline(0.0, 0.1, 0.9, 1.0, 1.0 - cfraction);
-                            }
-                        } else {
-                            // closer to the right edge
-                            double overlayXEnd = shineWidth - gap - 1;
-                            if ((topRightCornerRadius > 0.0) && (row <= (gap + topRightCornerRadius))) {
-                                // We are within the vertical span of the top-right corner
-                                double dy = gap + topRightCornerRadius - row;
-                                double dx = Math.sqrt(topRightCornerRadius * topRightCornerRadius - dy * dy);
-                                overlayXEnd = shineWidth - gap - 1 - topRightCornerRadius + dx;
-                            }
-                            if (col > overlayXEnd) {
-                                // trailing horizontal gap
-                                xalpha = 0.0;
-                            } else if (col > (overlayXEnd - ramp)) {
-                                // ramp-down to zero alpha horizontally
-                                double cfraction = (col - (overlayXEnd - ramp)) / ramp;
-                                xalpha = spline(0.0, 0.1, 0.9, 1.0, 1.0 - cfraction);
-                            }
-                        }
-
-                        int falpha = (int) (0.9 * rowAlpha * xalpha * yalpha);
-                        if (falpha < 0) {
-                            falpha = 0;
-                        }
-                        if (falpha > 255) {
-                            falpha = 255;
-                        }
-                        inPixels[col] = (falpha << 24) | rowRed << 16 | rowGreen << 8 | rowBlue;
-                    }
-                }
-                shineRaster.setDataElements(0, row, shineWidth, 1, inPixels);
-            }
+            BufferedImage shineImage = getShineImage(comp, contour, topShineColor, bottomShineColor,
+                    shineWidth, shineHeight);
 
             // Set rendering hints to favor speed over quality, since the visuals of the emulated
             // shine spot are subtle and don't have to be pixel perfect
@@ -211,16 +106,123 @@ public class StandardFillPainter implements RadianceFillPainter {
             graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
             graphics.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_SPEED);
 
-           graphics.drawImage(shineImage, 0, 0, iw, ih / 2, 0, 0, shineWidth, shineHeight, null);
+            graphics.clip(contour);
+            graphics.drawImage(shineImage, 0, 0, iw, ih / 2, 0, 0, shineWidth, shineHeight, null);
         }
 
         graphics.dispose();
     }
 
+    private BufferedImage getShineImage(Component comp, Shape contour,
+            Color topShineColor, Color bottomShineColor, int shineWidth, int shineHeight) {
+        BufferedImage shineImage = RadianceCoreUtilities.getBlankUnscaledImage(shineWidth, shineHeight);
+        double gap = 0.5 * RadianceCommonCortex.getScaleFactor(comp) / SCALE;
+        double ramp = 2 * RadianceCommonCortex.getScaleFactor(comp) / SCALE;
+
+        double topLeftCornerRadius = 0;
+        double topRightCornerRadius = 0;
+        if (contour instanceof RoundRectangle2D) {
+            // This matches the logic in RadianceOutlineUtilities.getBaseOutline
+            RoundRectangle2D rrContour = (RoundRectangle2D) contour;
+            topLeftCornerRadius = rrContour.getArcWidth() / (2.0 * SCALE);
+            topRightCornerRadius = rrContour.getArcWidth() / (2.0 * SCALE);
+        } else if (contour instanceof Ellipse2D) {
+            // This matches the logic in BladeIconUtils.drawRadioButton
+            Ellipse2D ellContour = (Ellipse2D) contour;
+            topLeftCornerRadius = ellContour.getWidth() / (2.0 * SCALE);
+            topRightCornerRadius = ellContour.getWidth() / (2.0 * SCALE);
+        }
+
+        WritableRaster shineRaster = shineImage.getRaster();
+
+        int[] inPixels = new int[shineWidth];
+        for (int row = 0; row < shineHeight; row++) {
+            if (row <= gap) {
+                // Leading vertical gap
+                for (int col = 0; col < shineWidth; col++) {
+                    inPixels[col] = 0x00000000;
+                }
+            } else {
+                // Get the interpolated shine color for this row
+                int rowColor = RadianceColorUtilities.getInterpolatedRGB(
+                        topShineColor, bottomShineColor, 1.0 - (double) row / (double) shineHeight);
+                int rowAlpha = (rowColor >>> 24) & 0xFF;
+                int rowRed = (rowColor >>> 16) & 0xFF;
+                int rowGreen = (rowColor >>> 8) & 0xFF;
+                int rowBlue = (rowColor >>> 0) & 0xFF;
+
+                // Compute the y-based alpha for all the pixels in this row
+                double yalpha;
+                if (row <= (gap + ramp)) {
+                    // Quick ramp-up
+                    double cfraction = (row - gap) / ramp;
+                    yalpha = spline(0.0, 0.1, 0.9, 1.0, cfraction);
+                } else {
+                    // slower ramp-down
+                    double cfraction = (row - gap - ramp) / (shineHeight - gap - ramp);
+                    yalpha = spline(0.0, 0.1, 0.9, 1.0, 1.0 - cfraction);
+                }
+
+                // For each column in this row, compute its x-based alpha
+                for (int col = 0; col < shineWidth; col++) {
+                    // x-alpha is based on the distance from left / right edges
+                    double xalpha = 1.0;
+                    if (col <= shineWidth / 2) {
+                        // closer to the left edge
+                        double overlayXStart = gap;
+                        if ((topLeftCornerRadius > 0.0) && (row <= (gap + topLeftCornerRadius))) {
+                            // We are within the vertical span of the top-left corner
+                            double dy = gap + topLeftCornerRadius - row;
+                            double dx = Math.sqrt(topLeftCornerRadius * topLeftCornerRadius - dy * dy);
+                            overlayXStart = gap + topLeftCornerRadius - dx;
+                        }
+                        if (col <= overlayXStart) {
+                            // leading horizontal gap
+                            xalpha = 0.0;
+                        } else if (col <= (overlayXStart + ramp)) {
+                            // ramp-up to full alpha horizontally
+                            double cfraction = (overlayXStart + ramp - col) / ramp;
+                            xalpha = spline(0.0, 0.1, 0.9, 1.0, 1.0 - cfraction);
+                        }
+                    } else {
+                        // closer to the right edge
+                        double overlayXEnd = shineWidth - gap - 1;
+                        if ((topRightCornerRadius > 0.0) && (row <= (gap + topRightCornerRadius))) {
+                            // We are within the vertical span of the top-right corner
+                            double dy = gap + topRightCornerRadius - row;
+                            double dx = Math.sqrt(topRightCornerRadius * topRightCornerRadius - dy * dy);
+                            overlayXEnd = shineWidth - gap - 1 - topRightCornerRadius + dx;
+                        }
+                        if (col > overlayXEnd) {
+                            // trailing horizontal gap
+                            xalpha = 0.0;
+                        } else if (col > (overlayXEnd - ramp)) {
+                            // ramp-down to zero alpha horizontally
+                            double cfraction = (col - (overlayXEnd - ramp)) / ramp;
+                            xalpha = spline(0.0, 0.1, 0.9, 1.0, 1.0 - cfraction);
+                        }
+                    }
+
+                    int falpha = (int) (0.9 * rowAlpha * xalpha * yalpha);
+                    if (falpha < 0) {
+                        falpha = 0;
+                    }
+                    if (falpha > 255) {
+                        falpha = 255;
+                    }
+                    inPixels[col] = (falpha << 24) | rowRed << 16 | rowGreen << 8 | rowBlue;
+                }
+            }
+            shineRaster.setDataElements(0, row, shineWidth, 1, inPixels);
+        }
+        return shineImage;
+    }
+
     /**
      * Computes the color of the top portion of the fill. Override to provide different visual.
      *
-     * @param fillScheme The fill scheme.
+     * @param fillScheme
+     *         The fill scheme.
      * @return The color of the top portion of the fill.
      */
     public Color getTopFillColor(RadianceColorScheme fillScheme) {
@@ -231,7 +233,8 @@ public class StandardFillPainter implements RadianceFillPainter {
      * Computes the color of the middle portion of the fill from the top. Override to provide
      * different visual.
      *
-     * @param fillScheme The fill scheme.
+     * @param fillScheme
+     *         The fill scheme.
      * @return The color of the middle portion of the fill from the top.
      */
     public Color getMidFillColorTop(RadianceColorScheme fillScheme) {
@@ -242,7 +245,8 @@ public class StandardFillPainter implements RadianceFillPainter {
      * Computes the color of the middle portion of the fill from the bottom. Override to provide
      * different visual.
      *
-     * @param fillScheme The fill scheme.
+     * @param fillScheme
+     *         The fill scheme.
      * @return The color of the middle portion of the fill from the bottom.
      */
     public Color getMidFillColorBottom(RadianceColorScheme fillScheme) {
@@ -252,7 +256,8 @@ public class StandardFillPainter implements RadianceFillPainter {
     /**
      * Computes the color of the bottom portion of the fill. Override to provide different visual.
      *
-     * @param fillScheme The fill scheme.
+     * @param fillScheme
+     *         The fill scheme.
      * @return The color of the bottom portion of the fill.
      */
     public Color getBottomFillColor(RadianceColorScheme fillScheme) {
@@ -262,7 +267,8 @@ public class StandardFillPainter implements RadianceFillPainter {
     /**
      * Computes the color of the top portion of the shine. Override to provide different visual.
      *
-     * @param fillScheme The fill scheme.
+     * @param fillScheme
+     *         The fill scheme.
      * @return The color of the top portion of the shine.
      */
     public Color getTopShineColor(RadianceColorScheme fillScheme) {
@@ -272,7 +278,8 @@ public class StandardFillPainter implements RadianceFillPainter {
     /**
      * Computes the color of the bottom portion of the shine. Override to provide different visual.
      *
-     * @param fillScheme The fill scheme.
+     * @param fillScheme
+     *         The fill scheme.
      * @return The color of the bottom portion of the shine.
      */
     public Color getBottomShineColor(RadianceColorScheme fillScheme) {
