e5100f6cdb65b7c9dececc00f27908f02887bb86
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2024-11-13 08:39:52 -0800

    Support macOS 15 / Sequoia

--- common/src/main/java/org/pushingpixels/radiance/common/internal/contrib/jgoodies/looks/LookUtils.java
+++ common/src/main/java/org/pushingpixels/radiance/common/internal/contrib/jgoodies/looks/LookUtils.java
@@ -118,7 +118,8 @@ public final class LookUtils {
 			|| startsWith(OS_VERSION, "10.13") || startsWith(OS_VERSION, "10.14")
 			|| startsWith(OS_VERSION, "10.15") || startsWith(OS_VERSION, "10.16")
             || startsWith(OS_VERSION, "11.") || startsWith(OS_VERSION, "12.")
-            || startsWith(OS_VERSION, "13.") || startsWith(OS_VERSION, "14."));
+            || startsWith(OS_VERSION, "13.") || startsWith(OS_VERSION, "14.")
+            || startsWith(OS_VERSION, "15."));
 
 	/**
 	 * True if this is Mac Mojave or later
@@ -127,7 +128,7 @@ public final class LookUtils {
 			&& (startsWith(OS_VERSION, "10.14") || startsWith(OS_VERSION, "10.15")
             || startsWith(OS_VERSION, "10.16") || startsWith(OS_VERSION, "11.")
             || startsWith(OS_VERSION, "12.") || startsWith(OS_VERSION, "13.")
-            || startsWith(OS_VERSION, "14."));
+            || startsWith(OS_VERSION, "14.") || startsWith(OS_VERSION, "15."));
 
 	/**
 	 * True if this is Mac Catalina or later
@@ -135,7 +136,8 @@ public final class LookUtils {
 	public static final boolean IS_OS_MAC_CATALINA_OR_LATER = IS_OS_MAC
 			&& (startsWith(OS_VERSION, "10.15") || startsWith(OS_VERSION, "10.16")
             || startsWith(OS_VERSION, "11.") || startsWith(OS_VERSION, "12.")
-            || startsWith(OS_VERSION, "13.") || startsWith(OS_VERSION, "14."));
+            || startsWith(OS_VERSION, "13.") || startsWith(OS_VERSION, "14.")
+            || startsWith(OS_VERSION, "15."));
 
 
 	// Other Properties *******************************************************
