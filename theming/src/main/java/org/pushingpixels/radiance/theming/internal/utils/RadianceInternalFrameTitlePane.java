fe446fd761688b9d386a0b36808a496117f86ac0
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2025-01-20 18:17:28 -0500

    Consistent appearance of tonal internal frame title panes
    For #400

--- theming/src/main/java/org/pushingpixels/radiance/theming/internal/utils/RadianceInternalFrameTitlePane.java
+++ theming/src/main/java/org/pushingpixels/radiance/theming/internal/utils/RadianceInternalFrameTitlePane.java
@@ -299,9 +299,7 @@ public class RadianceInternalFrameTitlePane extends BasicInternalFrameTitlePane 
                 // TODO: TONAL - finalize the text echo color logic
                 RadianceTextUtilities.paintTextWithDropShadow(this, graphics,
                     colorTokens.getOnContainer(),
-                    RadianceColorUtilities.getAlphaColor(
-                        colorTokens.getOnContainerVariant(),
-                        100),
+                    colorTokens.getInverseOnContainer(),
                     displayTitle, width, height, xOffset, yOffset);
             } else {
                 RadianceColorScheme scheme = RadianceCoreUtilities.getSkin(this.frame)
