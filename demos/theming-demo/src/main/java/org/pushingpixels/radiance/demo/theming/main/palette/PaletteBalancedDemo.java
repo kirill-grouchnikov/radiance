cb774d87dd4d6116bd41d10fb511c2f9a2c9c3f7
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2025-02-13 18:23:24 -0500

    Demo for bimodal dynamic palettes
    For #400

---
+++ demos/theming-demo/src/main/java/org/pushingpixels/radiance/demo/theming/main/palette/PaletteBalancedDemo.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2005-2025 Radiance Kirill Grouchnikov. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  o Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of the copyright holder nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.pushingpixels.radiance.demo.theming.main.palette;
+
+import com.jgoodies.forms.builder.FormBuilder;
+import com.jgoodies.forms.factories.Paddings;
+import org.pushingpixels.ephemeral.chroma.dynamiccolor.DynamicBimodalPalette;
+import org.pushingpixels.ephemeral.chroma.hct.Hct;
+import org.pushingpixels.radiance.demo.theming.main.RadianceLogo;
+import org.pushingpixels.radiance.theming.api.RadianceThemingCortex;
+import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
+import org.pushingpixels.radiance.theming.api.skin.BusinessSkin;
+
+import javax.swing.*;
+
+public class PaletteBalancedDemo extends JFrame {
+    public PaletteBalancedDemo() {
+        super("Chroma Palette");
+
+        RadianceLogo.tonalConfigureOn(this);
+
+        FormBuilder builder = FormBuilder.create().
+                columns("right:pref, 4dlu, fill:pref:grow, 4dlu, fill:pref:grow").
+                rows("p, $lg, p, $lg, p, $lg, p").
+                padding(Paddings.DIALOG);
+
+        int row = 1;
+
+        Hct seedOne = Hct.fromInt(0xFF00C1A9);
+        Hct seedTwo = Hct.fromInt(0xFF00D680);
+
+        DynamicBimodalPalette bimodalPalette1 = DynamicBimodalPalette.balanced(
+            /* seedOne */ seedOne,
+            /* seedTwo */ seedTwo,
+            /* isDark */ false,
+            /* isContrastLevel */ 0.0);
+
+        DynamicBimodalPalette bimodalPalette2 = DynamicBimodalPalette.balanced(
+            /* seedOne */ seedTwo,
+            /* seedTwo */ seedOne,
+            /* isDark */ false,
+            /* isContrastLevel */ 0.0);
+
+        builder.addROLabel("Palette one").xy(1, row)
+            .add(new TonalPalettePreview(bimodalPalette1.paletteOne))
+            .xyw(3, row, 3);
+        row += 2;
+        builder.addROLabel("Palette two").xy(1, row)
+            .add(new TonalPalettePreview(bimodalPalette1.paletteTwo))
+            .xyw(3, row, 3);
+        row += 2;
+        builder.addROLabel("Bimodal one-two").xy(1, row)
+            .add(new BimodalPalettePreview(bimodalPalette1))
+            .xyw(3, row, 3);
+        row += 2;
+        builder.addROLabel("Bimodal two-one").xy(1, row)
+            .add(new BimodalPalettePreview(bimodalPalette2))
+            .xyw(3, row, 3);
+
+        this.add(builder.build());
+
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            RadianceThemingCortex.GlobalScope.setFocusKind(RadianceThemingSlices.FocusKind.NONE);
+            RadianceThemingCortex.GlobalScope.setSkin(new BusinessSkin.BusinessTonalSkin());
+            new PaletteBalancedDemo().setVisible(true);
+        });
+    }
+}
