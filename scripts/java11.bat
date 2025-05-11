afa68e23c6e987e9216821ef6217d2d586ab3f21
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2025-05-11 17:47:07 -0400

    Publishing to Central

---
+++ scripts/java11.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.9
+echo setting PATH
+set PATH=C:\Program Files\Java\jdk-11.0.9\bin;%PATH%
+echo Display java version
+java -version
