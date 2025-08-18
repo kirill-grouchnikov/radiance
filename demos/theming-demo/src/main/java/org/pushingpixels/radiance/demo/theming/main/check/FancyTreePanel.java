eef7ea723ff67f07d09f35415d55c8166e9d76cc
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2025-08-18 16:42:39 -0400

    Second pass for migrating panel-based renderers
    Convert the panel tree cell renderer to a single `bindRenderer` callback that gets `ContainerColorTokens`. Updated the demo usages with no visual changes yet.
    
    For #525

--- demos/theming-demo/src/main/java/org/pushingpixels/radiance/demo/theming/main/check/FancyTreePanel.java
+++ demos/theming-demo/src/main/java/org/pushingpixels/radiance/demo/theming/main/check/FancyTreePanel.java
@@ -30,7 +30,9 @@
 package org.pushingpixels.radiance.demo.theming.main.check;
 
 import com.jgoodies.forms.builder.FormBuilder;
+import org.pushingpixels.radiance.common.api.icon.RadianceIcon;
 import org.pushingpixels.radiance.demo.theming.main.check.svg.person_outline_black_24dp;
+import org.pushingpixels.radiance.theming.api.ContainerColorTokens;
 import org.pushingpixels.radiance.theming.api.RadianceThemingCortex;
 import org.pushingpixels.radiance.theming.api.renderer.RadiancePanelTreeCellRenderer;
 
@@ -108,21 +110,12 @@ public class FancyTreePanel extends ControllablePanel {
             this.add(secondRow.build());
             this.add(thirdRow.build());
 
-            // Register the text labels so that they get the right colors on rollover,
-            // selection and other highlight effects
-            this.registerThemeAwareLabelsWithText(this.fromLabel, this.timeLabel, this.titleLabel,
-                    this.summaryLabel, this.unreadLabel);
-            // Register the icon factory for the person label here since the icon visuals
-            // are the same for all the thread rows in this app
-            this.registerThemeAwareLabelWithIcon(this.personLabel,
-                    person_outline_black_24dp.factory(),
-                    new Dimension(10, 10));
-
             this.setOpaque(false);
         }
 
         @Override
-        protected void bindData(JTree tree, Object value, int row) {
+        protected void bindRenderer(JTree tree, Object value, int row, ContainerColorTokens colorTokens) {
+            // Bind data
             DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
             Object userObject = node.getUserObject();
             if (userObject == null) {
@@ -144,6 +137,20 @@ public class FancyTreePanel extends ControllablePanel {
                     this.unreadLabel.setVisible(false);
                 }
             }
+
+            // Configure colors
+            this.fromLabel.setForeground(colorTokens.getOnContainer());
+            this.timeLabel.setForeground(colorTokens.getOnContainer());
+            this.titleLabel.setForeground(colorTokens.getOnContainer());
+            this.summaryLabel.setForeground(colorTokens.getOnContainer());
+            this.unreadLabel.setForeground(colorTokens.getOnContainer());
+
+            // And icons
+            RadianceIcon personIcon = person_outline_black_24dp.factory().createNewIcon();
+            personIcon.setColorFilter(color -> colorTokens.getOnContainer());
+            personIcon.setDimension(new Dimension(10, 10));
+            this.personLabel.setIcon(personIcon);
+
         }
     }
 
--- theming/src/main/java/org/pushingpixels/radiance/theming/api/renderer/RadiancePanelTreeCellRenderer.java
+++ theming/src/main/java/org/pushingpixels/radiance/theming/api/renderer/RadiancePanelTreeCellRenderer.java
@@ -29,21 +29,21 @@
  */
 package org.pushingpixels.radiance.theming.api.renderer;
 
-import org.pushingpixels.radiance.common.api.icon.RadianceIcon;
 import org.pushingpixels.radiance.theming.api.ComponentState;
 import org.pushingpixels.radiance.theming.api.ContainerColorTokens;
 import org.pushingpixels.radiance.theming.api.RadianceThemingCortex;
 import org.pushingpixels.radiance.theming.api.RadianceThemingSlices;
 import org.pushingpixels.radiance.theming.internal.animation.StateTransitionTracker;
+import org.pushingpixels.radiance.theming.internal.blade.BladeContainerColorTokens;
+import org.pushingpixels.radiance.theming.internal.blade.BladeUtils;
 import org.pushingpixels.radiance.theming.internal.ui.RadianceTreeUI;
 import org.pushingpixels.radiance.theming.internal.utils.CoreColorTokenUtils;
 
 import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
 import javax.swing.plaf.TreeUI;
 import javax.swing.tree.TreeCellRenderer;
 import java.awt.*;
-import java.util.*;
+import java.util.Map;
 
 /**
  * Base class for tree renderers that are more complex than what is provided by
@@ -51,78 +51,27 @@ import java.util.*;
  * highlight visuals and animations under all Radiance skins. The flow of layout and
  * data is:
  *
- * <ul>
- * <li>In the constructor, add all sub-components to your renderer and define the
- * layout logic.</li>
- * <li>In {@link #bindData(JTree, Object, int)} bind the relevant data fields to those
- * sub-components.</li>
- * <li>Use {@link #registerThemeAwareLabelsWithText(JLabel...)} in the constructor
- * to "mark" those sub-component labels that should participate in highlight animations
- * on their text based on the current state (rollover, selection, etc). In case a specific
- * label is using a fixed foreground / text color for some of the rows in your tree, use
- * {@link #registerThemeAwareLabelsWithText(JLabel...)} and
- * {@link #unregisterThemeAwareLabelsWithText(JLabel...)} in
- * {@link #onPreRender(JTree, Object, int)} instead of in the constructor.</li>
- * <li>Use {@link #registerThemeAwareLabelWithIcon(JLabel, RadianceIcon.Factory, Dimension)}
- * in the constructor to "mark" those sub-component labels that should participate in highlight
- * animations on their icons based on the current state (rollover, selection, etc). In case a
- * specific label is using a fixed icon for some of the rows in your tree or different icon
- * sources for different rows, use
- * {@link #registerThemeAwareLabelWithIcon(JLabel, RadianceIcon.Factory, Dimension)} and
- * {@link #unregisterThemeAwareLabelWithIcon(JLabel)} in
- * {@link #onPreRender(JTree, Object, int)} instead of in the constructor.</li>
- * </ul>
- * <p>
+ * The abstract {@link #bindRenderer(JTree, Object, int, ContainerColorTokens)} will be called
+ * during the drawing phase, passing the color tokens that match the current tree row state.
+ * In your implementation of this method, bind the data (text, icon, etc), as well as colors.
+ *
  * Note that {@link #getTreeCellRendererComponent(JTree, Object, boolean, boolean, boolean, int, boolean)}
  * in this class is marked as final and can not be overriden in the application code.
  */
 @RadianceRenderer
 public abstract class RadiancePanelTreeCellRenderer extends JPanel
         implements TreeCellRenderer {
-    private static class IconData {
-        private RadianceIcon.Factory iconFactory;
-        private Dimension iconSize;
-
-        private IconData(RadianceIcon.Factory iconFactory, Dimension iconSize) {
-            this.iconFactory = iconFactory;
-            this.iconSize = iconSize;
-        }
-    }
-
-    private Set<JLabel> themeAwareLabels;
-    private Map<JLabel, IconData> themeAwareLabelsWithIcons;
+    private BladeContainerColorTokens mutableContainerTokens = new BladeContainerColorTokens();
 
     public RadiancePanelTreeCellRenderer() {
         RadianceThemingCortex.ComponentOrParentChainScope.setColorizationFactor(this, 1.0);
-        this.themeAwareLabels = new HashSet<>();
-        this.themeAwareLabelsWithIcons = new HashMap<>();
-    }
-
-    protected void registerThemeAwareLabelsWithText(JLabel... labels) {
-        this.themeAwareLabels.addAll(Arrays.asList(labels));
-    }
-
-    protected void unregisterThemeAwareLabelsWithText(JLabel... labels) {
-        for (JLabel label : labels) {
-            this.themeAwareLabels.remove(label);
-        }
-    }
-
-    protected void registerThemeAwareLabelWithIcon(JLabel label,
-            RadianceIcon.Factory radianceIconFactory, Dimension iconDimension) {
-        this.themeAwareLabelsWithIcons.put(label,
-                new IconData(radianceIconFactory, iconDimension));
-    }
-
-    protected void unregisterThemeAwareLabelWithIcon(JLabel label) {
-        this.themeAwareLabelsWithIcons.remove(label);
     }
 
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
         this.setComponentOrientation(tree.getComponentOrientation());
 
-        Color labelForeground;
+        ContainerColorTokens colorTokensForRenderer;
         TreeUI treeUI = tree.getUI();
         if (treeUI instanceof RadianceTreeUI) {
             RadianceTreeUI ui = (RadianceTreeUI) treeUI;
@@ -142,74 +91,38 @@ public abstract class RadiancePanelTreeCellRenderer extends JPanel
                 Map<ComponentState, StateTransitionTracker.StateContributionInfo> activeStates =
                         modelStateInfo.getStateContributionMap();
                 if (currState.isDisabled() || (activeStates == null) || (activeStates.size() == 1)) {
-                    ContainerColorTokens colorTokens = getContainerTokensForState(tree, ui, currState);
-                    labelForeground = new ColorUIResource(colorTokens.getOnContainer());
+                    colorTokensForRenderer = getContainerTokensForState(tree, ui, currState);
                 } else {
-                    float aggrRed = 0;
-                    float aggrGreen = 0;
-                    float aggrBlue = 0;
-
-                    for (Map.Entry<ComponentState, StateTransitionTracker.StateContributionInfo> activeEntry
-                        : modelStateInfo.getStateContributionMap().entrySet()) {
-                        ComponentState activeState = activeEntry.getKey();
-                        float contribution = activeEntry.getValue().getContribution();
-
-                        ContainerColorTokens colorTokens = getContainerTokensForState(tree, ui, activeState);
-                        Color foreground = colorTokens.getOnContainer();
-                        aggrRed += foreground.getRed() * contribution;
-                        aggrGreen += foreground.getGreen() * contribution;
-                        aggrBlue += foreground.getBlue() * contribution;
-                    }
-                    labelForeground = new ColorUIResource(
-                            new Color((int) aggrRed, (int) aggrGreen, (int) aggrBlue));
+                    BladeUtils.populateColorTokens(
+                        mutableContainerTokens, tree, modelStateInfo, currState,
+                        RadianceThemingSlices.ContainerColorTokensAssociationKind.DEFAULT,
+                        false, false, CoreColorTokenUtils.ContainerType.NEUTRAL);
+                    colorTokensForRenderer = mutableContainerTokens;
                 }
             } else {
-                ContainerColorTokens colorTokens = getContainerTokensForState(tree, ui, currState);
+                colorTokensForRenderer = getContainerTokensForState(tree, ui, currState);
                 if (isDropLocation) {
-                    colorTokens = CoreColorTokenUtils.getContainerTokens(tree,
+                    colorTokensForRenderer = CoreColorTokenUtils.getContainerTokens(tree,
                         RadianceThemingSlices.ContainerColorTokensAssociationKind.HIGHLIGHT,
                         currState, CoreColorTokenUtils.ContainerType.NEUTRAL);
                 }
-                labelForeground = new ColorUIResource(colorTokens.getOnContainer());
             }
         } else {
-            if (selected) {
-                labelForeground = UIManager.getColor("Tree.selectionForeground");
-            } else {
-                labelForeground = UIManager.getColor("Tree.textForeground");
-            }
+            colorTokensForRenderer = CoreColorTokenUtils.getContainerTokens(tree,
+                selected ? ComponentState.SELECTED : ComponentState.ENABLED,
+                CoreColorTokenUtils.ContainerType.NEUTRAL);
         }
 
         this.setEnabled(tree.isEnabled());
         this.setFont(tree.getFont());
 
-        this.bindData(tree, value, row);
-
-        this.onPreRender(tree, value, row);
-
-        for (JLabel themeAwareLabel : this.themeAwareLabels) {
-            themeAwareLabel.setForeground(labelForeground);
-        }
-
-        for (Map.Entry<JLabel, IconData> themeAwareLabelsWithIcons :
-                this.themeAwareLabelsWithIcons.entrySet()) {
-            JLabel label = themeAwareLabelsWithIcons.getKey();
-            IconData iconData = themeAwareLabelsWithIcons.getValue();
-
-            RadianceIcon icon = iconData.iconFactory.createNewIcon();
-            icon.setColorFilter(color -> labelForeground);
-            icon.setDimension(iconData.iconSize);
-
-            label.setIcon(icon);
-        }
+        this.bindRenderer(tree, value, row, colorTokensForRenderer);
 
         return this;
     }
 
-    protected abstract void bindData(JTree tree, Object value, int row);
-
-    protected void onPreRender(JTree tree, Object value, int row) {
-    }
+    protected abstract void bindRenderer(JTree tree, Object value, int row,
+        ContainerColorTokens colorTokens);
 
     private ContainerColorTokens getContainerTokensForState(JTree tree, RadianceTreeUI ui,
         ComponentState activeState) {
